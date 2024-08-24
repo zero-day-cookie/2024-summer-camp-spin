a395942939bb2c34a5a2c99db1eb96f6877b43dc
xiung <steven19790906@gmail.com>
2024-08-25 00:07:03 +0800

    Init

---
+++ script.js
@@ -0,0 +1,8 @@
+let container = document.querySelector(".circle");
+let btn = document.getElementById("spin");
+let number = Math.ceil(Math.random() * 1000);
+
+btn.onclick = function () {
+	container.style.transform = "rotate(" + number + "deg)";
+	number += Math.ceil(Math.random() * 1000);
+}
